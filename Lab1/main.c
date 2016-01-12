432ef1dc1769019fc5d6b1f7b74b1a9138005c5c
Connor Robert Kenny <connork@lnxsrv04.seas.ucla.edu>
2016-01-12 10:28:20 -0800

    Tuesday morining

--- Lab1/main.c
+++ Lab1/main.c
@@ -9,11 +9,11 @@
 int main(int argc, char* argv[])
 {
   
-  const static struct option long_opts[] =
+  static const struct option long_opts[] =
     {
       {"rdonly",   required_argument,   0, 'a'},
       {"wronly",   required_argument,   0, 'b'},
-      {"command",  required_argument,   0, 'c'},
+      {"command",  no_argument,         0, 'c'},
       {"verbose",  no_argument,         0, 'd'},
       {0,0,0,0},
     };
@@ -22,6 +22,8 @@ int main(int argc, char* argv[])
   int option_index = 0;
   int iarg = 0;
   int verbose_flag = 0;
+  int exit_status = 0;
+  int ret;
   
   while (iarg != -1)
     {
@@ -30,15 +32,11 @@ int main(int argc, char* argv[])
       if (iarg == -1)
 	break;
 
+      if (verbose_flag == 1)
+	printf("--%s %s\n", long_opts[iarg-'a'].name, optarg);
+
       if (iarg == '?')
 	continue;
-
-      if (verbose_flag == 1)
-	{
-	  // Check which is right later
-	  //	  printf("%s", argv[optind-1]);
-	  printf("--%s %s\n", long_opts[iarg-'a'].name, optarg);
-	}
       
       // - is standard input
       // fileno
@@ -46,25 +44,45 @@ int main(int argc, char* argv[])
       switch (iarg)
 	{
 	case 'a':
-	  // Error check all of these
-	  open(optarg, O_RDONLY);
+	  ret = open(optarg, O_RDONLY);
+	  if (ret < 0)
+	    fprintf(stderr, "File error\n");
 	  break;
 	case 'b':
-	  open(optarg, O_WRONLY);
+	  ret = open(optarg, O_WRONLY);
+	  if (ret < 0)
+	    fprintf(stderr, "File error\n");
 	  break;
 	case 'c':
+	  pid_t cPID = fork();
+	  if (argc - optind < 4)
+	    fprintf(stderr, "File descriptor error\n");
+	  
+	  int fdi = argv[optind++];
+	  int fdo = argv[optind++];
+	  int fde = argv[optind++];
+	  char *command = argv[optind++];
+      	  if (cPID >= 0)
+	    {
+	      if (cPID == 0) // Child process
+		{
+		  execvp(command, argv[optind]); // Put in the command in first, args in second
+		}
+	    }
+	  else
+	    {
+	      fprintf(stderr, "Could not create child process");
+	      // fork failed
+	    }
 	  break;
 	case 'd':
 	  verbose_flag = 1;
 	  break;
 	default:
-	  fprintf(stderr, "Invalid option - %d", iarg);
-	  exit(1);
-	  break;
+	  // No default case required, caught by the ? above switch
 	}
     }
-
+  return 0;
 }
-
 
 // CLOSE THE FILES
