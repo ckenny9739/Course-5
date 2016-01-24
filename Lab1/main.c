1dbcb52d11d81cced4e2ed7de9d3b258038129c3
Connor Robert Kenny <connork@lnxsrv04.seas.ucla.edu>
2016-01-23 21:47:20 -0800

    1/23

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,8 +1,8 @@
 /*
   To Do List:
   - Check the mode for open
-  - Try to reproduce the file error
   - Check stdin, stdout, stderr
+  - pipe, wait
  */
 
 
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <signal.h>
+
 
 int check_size(void* arr, int arr_ind, unsigned int *arr_size)
 {
@@ -29,6 +31,17 @@ int check_size(void* arr, int arr_ind, unsigned int *arr_size)
   return 0;
 }
 
+void simpsh_handler(int signum)
+{
+  fprintf(stderr, "%d caught\n", signum);
+  exit(signum);
+}
+
+struct proc {
+  pid_t id;
+  char argArr[512];
+};
+struct proc procArr[32];
 
 // Main
 
@@ -38,7 +51,7 @@ int main(int argc, char* argv[])
   static const struct option long_opts[] =
     {
       {"rdonly",    required_argument,   0, 'a'},
-      {"wronly",    required_argument,   0, 'b'},
+      {"wronly",   required_argument,   0, 'b'},
       {"command",   required_argument,   0, 'c'},
       {"verbose",   no_argument,         0, 'd'},
       {"append",    no_argument,         0, 'e'},
@@ -54,9 +67,16 @@ int main(int argc, char* argv[])
       {"trunc",     no_argument,         0, 'o'},
       {"rdwr",      required_argument,   0, 'p'},
       {"pipe",      no_argument,         0, 'q'},
+      {"abort",     no_argument,         0, 'r'},
+      {"close",     required_argument,   0, 's'},
+      {"catch",     required_argument,   0, 't'},
+      {"ignore",    required_argument,   0, 'u'},
+      {"default",   required_argument,   0, 'v'},
+      {"pause",     no_argument,         0, 'w'},
+      {"wait",      no_argument,         0, 'x'},
       {0,0,0,0},
     };
-  
+
   int bit_mask = 0;
   int option_index = 0;
   int iarg = 0;
@@ -64,8 +84,12 @@ int main(int argc, char* argv[])
   int fd_ind = 0;
   unsigned int fd_size = 128;
   int* file_descriptors = (int*) malloc(fd_size*sizeof(int));
+  file_descriptors[0] = file_descriptors[1] = file_descriptors[2] = -1;
+  // Set as -1 to notify if using stdin, stdout, stderr
   int fdi, fdo, fde;
   int ret;
+  int pCount = 0;
+  int waitFlag = 0;
   pid_t cPID;
   int arg_ind = 0;
   unsigned int arg_size = 512;
@@ -77,7 +101,7 @@ int main(int argc, char* argv[])
     {
       j = 0;
       //printf("argv has %s\n", argv[optind]);
-      iarg = getopt_long(argc, argv, "a:b:c:defghijklmnop:q", long_opts, &option_index);
+      iarg = getopt_long(argc, argv, "a:b:c:defghijklmnop:qrstuvwx", long_opts, &option_index);
       //printf("iarg : %d\n", iarg);
       if (iarg == -1)
 	break;
@@ -154,7 +178,7 @@ int main(int argc, char* argv[])
 	    fprintf(stderr, "Not a file descriptor\n");
 	    break;
 	  }
-	  
+
 	  //printf("optind is pointing to %s\n", argv[optind]);
 	  //printf("optind is pointing to %s\n", argv[optind]);
 	  //printf("optind is pointing to %s\n", argv[optind]);
@@ -170,7 +194,7 @@ int main(int argc, char* argv[])
 	      optind++;
 	    }
 	  args[j] = NULL;
-	  if (fdi > fd_ind || fdo > fd_ind || fde > fd_ind) {
+	  if (fdi < 0 || (fdi > fd_ind && fdi > 2) || fdo < 0 || (fdo > fd_ind && fdo > 2) || fde < 0 || (fde > fd_ind && fde > 2)) {
 	    fprintf(stderr, "Invalid file descriptor\n");
 	    break;
 	  }
@@ -181,33 +205,51 @@ int main(int argc, char* argv[])
 	      if (cPID == 0) // Child process
 		{
 		  //printf("Child Process: %s\n", args[0]);
-		  if (dup2(file_descriptors[fdi], 0) == -1) {
-		    fprintf(stderr, "Invalid file descriptor - input");
-		    break;
-		  }
-		  if (dup2(file_descriptors[fdo], 1) == -1) {
-		    fprintf(stderr, "Invalid file descriptor - output");
-		    break;
-		  }
-		  if (dup2(file_descriptors[fde], 2) == -1) {
-		    fprintf(stderr, "Invalid file descriptor - error");
-		    break;
-		  }
+		  if (fdi >=3 || (fdi < 3 && file_descriptors[fdi] != -1))
+		    if (dup2(file_descriptors[fdi], 0) == -1) {
+		      fprintf(stderr, "Dup2 error - input");
+		      break;
+		    }
+		  if (fdo >= 3 || (fdo < 3 && file_descriptors[fdo] != -1))
+		    if (dup2(file_descriptors[fdo], 1) == -1) {
+		      fprintf(stderr, "Dup2 error - output");
+		      break;
+		    }
+		  if (fde >= 3 || (fde < 3 && file_descriptors[fde] != -1))
+		    if (dup2(file_descriptors[fde], 2) == -1) {
+		      fprintf(stderr, "Dup2 error - error");
+		      break;
+		    }
 		  // Close File Descriptors if not equal to the correct value
-		  
+		  for (i = 0; i < fd_ind; i++) {
+		    if (i != fdi && i != fdo && i != fde)
+		      close(file_descriptors[i]);
+		  }
 
 		  
 		  //  printf("Round 2: input: %d, output: %d, error: %d\n", fdi, fdo, fde);
-		  //  printf("Command: %s\n", args[0]);
-		  //  printf("Arguments: %s\n", args[0]);
-		  //  printf("Arguments: %s\n", args[1]);
-		  //  printf("Arguments: %s\n", args[2]);
+		  //printf("Command: %s\n", args[0]);
+		  printf("Arguments: %s\n", args[0]);
+		  printf("Arguments: %s\n", args[1]);
+		  printf("Arguments: %s\n", args[2]);
+		  printf("Arguments: %s\n", args[3]);
+		  printf("Execvp running\n");
 		  execvp(args[0], args);
+		  printf("Execvp returned\n");
 		  // Put in the command name in first, args (including args[0] as name) in second
 		  // Error check for error in execvp
 		  fprintf(stderr, "execvp error\n");
 		  exit(1); // Something else here????
 		}
+	      else {
+		procArr[pCount].id = cPID;
+		strcat(procArr[pCount].argArr, args[0]);
+		for (i = 1; i < j; i++) {
+		  strcat(procArr[pCount].argArr, " ");
+		  strcat(procArr[pCount].argArr, args[i]);
+		}
+		pCount++;
+	      }
 	   }
 	  else
 	    {
@@ -270,19 +312,72 @@ int main(int argc, char* argv[])
 	  //  printf("Finished Read/Write\n");
 	  break;
 	case 'q':
-	  ret = check_size(file_descriptors, fd_ind, (&fd_size));
+	  ret = check_size(file_descriptors, fd_ind + 1, (&fd_size));
 	  if (ret == -1) {
 	    fprintf(stderr, "Memory allocation error for file descriptors\n");
 	    break;
 	  }
-	  // FILL IN REST OF PIPE HERE (Check out the size 2 array.. Do we insert like {file_descriptors[ind++] x2}
-	  // Do we need to call check_size again because multiple fd?
-	  bit_mask = 0;
-	  
+	  int pipe_fd[2];
+	  ret = pipe(pipe_fd);
+	  if (ret < 0) {
+	    fprintf(stderr, "Pipe failed - %d\n", ret);
+	    break;
+	  }
+	  file_descriptors[fd_ind++] = pipe_fd[0];
+	  file_descriptors[fd_ind++] = pipe_fd[1];
+	  break;
+	case 'r':
+	  raise(SIGSEGV);
+	  break;
+	case 's':
+	  if(sscanf(optarg, "%i", &ret) == -1) {
+	    fprintf(stderr, "Error closing\n");
+	    break;
+	  }
+	  close(file_descriptors[ret]);
+	  break;
+	case 't':
+	  if(sscanf(optarg, "%i", &ret) == -1) {
+	    fprintf(stderr, "Error catching\n");
+	    break;
+	  }
+	  signal(ret, simpsh_handler);
+	  break;
+	case 'u':
+	  if(sscanf(optarg, "%i", &ret) == -1) {
+	    fprintf(stderr, "Error catching\n");
+	    break;
+	  }
+	  signal(ret, SIG_IGN);
+	  break;
+	case 'v':
+	  if(sscanf(optarg, "%i", &ret) == -1) {
+	    fprintf(stderr, "Error catching\n");
+	    break;
+	  }
+	  signal(ret, SIG_DFL);
+	  break;
+	case 'w':
+	  pause();
 	  break;
 	 // No default case required, caught by the ? above switch
+	case 'x':
+	  waitFlag = 1;
+	  break;
 	}
     }
   //printf("Main returned\n");
+  if(waitFlag) {
+    int process = 0;
+    int stat;
+    int k = 0;
+    for(process; process < pCount; process++) {
+      ret = wait(&stat);
+      for(k; k < pCount; k++)
+	if (procArr[k].id == ret)
+	  printf("%d %s\n", WEXITSTATUS(stat), procArr[k].argArr);
+    }
+  }
+  //  printf("Main returned\n");
   return 0;
 }
