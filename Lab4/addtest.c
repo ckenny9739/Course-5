3a4085a0fb4c1e145ad388278a972b149188e88b
Connor Robert Kenny <connork@lnxsrv02.seas.ucla.edu>
2016-03-08 00:50:10 -0800

    L4 P1 almost done

--- Lab4/addtest.c
+++ Lab4/addtest.c
@@ -5,31 +5,6 @@
 #include <getopt.h>
 #include <pthread.h>
 
-/*
-  1.2
-  mutex
-  Global --> pthread_mutex_t test_mutex;
-
-  pthread_mutex_init(&test_mutex, NULL);
-
-  pthread_mutex_lock(&test_mutex);
-  add(...)
-  pthread_mutex_unlock(&test_mutex);
-
-
-  spinlock
-
-  GLOBAL --> int test_lock = 0;
-  while(__sync_lock_test_and_set(&test_lock, 1));
-  add();
-  __sync_lock_release(&test_lock);
-
-
-  
-
- */
-
-
 // Global variables
 long long count = 0;
 int num_iterations = 1;
@@ -37,26 +12,67 @@ int num_threads = 1;
 pthread_mutex_t test_mutex;
 int test_lock = 0;
 int opt_yield = 0;
+char sync = '\0';
 
 void add(long long *pointer, long long value) {
   long long sum = *pointer + value;
   if(opt_yield) {
     pthread_yield();
-  }
+    }
   *pointer = sum;
 }
 
 void threadFunc() {
   int i;
   //  printf("1 Thread Func: %d\n", num_iterations);
-  for (i = 0; i < num_iterations; i++)
+  for (i = 0; i < num_iterations; i++) {
+    if(sync == 'm') {
+      pthread_mutex_lock(&test_mutex);
+    }
+    else if(sync == 's') {
+      while(__sync_lock_test_and_set(&test_lock, 1))
+	continue;
+    }
+    else if(sync == 'c') {
+      while(__sync_bool_compare_and_swap(&test_lock, 0, 1) == 0)
+	continue;
+    }
+    
     add(&count, 1);
 
-  //  printf("2 Thread Func\n");
-  
-  for (i = 0; i < num_iterations; i++)
-    add(&count, -1);
+    if(sync == 'm') {
+      pthread_mutex_unlock(&test_mutex);
+    }
+    else if(sync == 's') {
+      __sync_lock_release(&test_lock);
+    }
+    else if(sync == 'c') {
+      __sync_bool_compare_and_swap(&test_lock, 1, 0);
+    }
+  }
 
+   
+  for (i = 0; i < num_iterations; i++) {
+    if(sync == 'm') 
+      pthread_mutex_lock(&test_mutex);
+    
+    else if(sync == 's') 
+      while(__sync_lock_test_and_set(&test_lock, 1))
+	continue;
+    
+    else if(sync == 'c') 
+      while(__sync_bool_compare_and_swap(&test_lock, 0, 1) == 0)
+	continue;
+    
+    add(&count, -1);
+    
+    if(sync == 'm')
+      pthread_mutex_unlock(&test_mutex);
+    else if(sync == 's') 
+      __sync_lock_release(&test_lock);
+    else if(sync == 'c') 
+      __sync_bool_compare_and_swap(&test_lock, 1, 0);
+  }
   //  printf("3 Thread Func\n");
 }
 
@@ -71,39 +87,67 @@ elapsed time = end time - start time
 int main(int argc, char* argv[]) {
 
   // Use options here...
-  if (argc > 4) {
+  if (argc > 5) {
     fprintf(stderr, "Incorrect options\n");
     exit(1);
   }
   while (1) {
     struct option long_options[] =
       {
-	{"threads", optional_argument, 0, 'a'},
-	{"iterations", optional_argument, 0, 'b'},
-	{"yield", optional_argument, 0, 'c'},
+	{"threads", required_argument, 0, 'a'},
+	{"iterations", required_argument, 0, 'b'},
+	{"yield", required_argument, 0, 'c'},
+	{"sync", required_argument, 0, 'd'},
 	{0, 0, 0, 0}
       };
     int option_index = 0;
-    int c = getopt_long(argc, argv, "a::b::c::", long_options, &option_index);
+    int c = getopt_long(argc, argv, "a:b:c:d:", long_options, &option_index);
 
     if (c == -1)
       break;
-
+    char in;
+    int ret;
     switch(c) {
       case 'a':
-	num_threads = (int) strtol(optarg, NULL, 10);
+	ret = (int) strtol(optarg, NULL, 10);
+	if (ret <= 0) {
+	  fprintf(stderr, "Number of threads must be positive\n");
+	  exit(1);
+	}
+	num_threads = ret;
 	break;
       case 'b':
-	num_iterations = (int) strtol(optarg, NULL, 10);
+	ret = (int) strtol(optarg, NULL, 10);
+	if (ret <= 0) {
+	  fprintf(stderr, "Number of iterations must be positive\n");
+	  exit(1);
+	}
+	num_iterations = ret;
 	break;
       case 'c':
-	opt_yield = (int) strtol(optarg, NULL, 10);
+	ret = (int) strtol(optarg, NULL, 10);
+	if (ret != 1) {
+	  fprintf(stderr, "Yield option argument must be 1\n");
+	  exit(1);
+	}
+	opt_yield = 1;
+	break;
+      case 'd':
+        in = *optarg;
+	if(in == 's' || in == 'm' || in == 'c') {
+	  sync = *optarg;
+	}
+	else {
+	  fprintf(stderr, "Incorrect sync options\n");
+	  exit(1);
+	}
 	break;
       default:
 	fprintf(stderr, "Incorrect options\n");
 	exit(1);
     }
   }
+  pthread_mutex_init(&test_mutex, NULL);
   
   struct timespec sTime;
   int start = clock_gettime(CLOCK_MONOTONIC, &sTime);
@@ -144,16 +188,17 @@ int main(int argc, char* argv[]) {
   }
 
 
-  long int ns = eTime.tv_nsec - sTime.tv_nsec;
+  unsigned long long ns = eTime.tv_nsec - sTime.tv_nsec;
   int carry = 0;
   if(ns < 0) {
     ns += 1000000000;
     carry = 1;
   }
-  long int sec = eTime.tv_sec - sTime.tv_sec - carry;
-  fprintf(stdout, "elapsed time: %d ns\n", sec*1000000000 + ns);
+  unsigned long long sec = eTime.tv_sec - sTime.tv_sec - carry;
+  double elapsed = sec*1000000000 + ns;
+  fprintf(stdout, "elapsed time: %.0f ns\n", elapsed);
 
-  fprintf(stdout, "per operation: %f ns\n", (double)(sec*1000000000 + ns)/num_ops);
+  fprintf(stdout, "per operation: %f ns\n", elapsed/num_ops);
 
   exit(0);
 }
