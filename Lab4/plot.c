547b206aca3cef6c7a5b8707c903fafa7fd398e9
Connor Robert Kenny <connork@lnxsrv04.seas.ucla.edu>
2016-03-09 21:09:36 -0800

    Lab 4 - Part 1 + 3 completed

--- Lab4/plot.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-  FILE *pipe = popen("gnuplot -persist","w");
-  fprintf(pipe, "set data style lines\n");
-  fprintf(pipe, "plot 'yourfile.dat' using 1:2\n");
-  close(pipe);
-}
